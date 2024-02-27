ffb1eb9c229455ecd1bfe6dac1b11087c9c5f6d7
KaizenLol <122132830+KaizenLol@users.noreply.github.com>
2024-02-27 16:39:57 +0100

    clipboard fix

--- init.lua
+++ init.lua
@@ -47,6 +47,7 @@ vim.g.maplocalleader = ' '
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- vim.api.nvim_set_option("clipboard","unnamed")
 -- Custom behavior for Ctrl+D and Ctrl+U in normal mode
 
 
@@ -131,81 +132,81 @@ require('lazy').setup({
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
-  {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map({ 'n', 'v' }, ']c', function()
-          if vim.wo.diff then
-            return ']c'
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to next hunk' })
-
-        map({ 'n', 'v' }, '[c', function()
-          if vim.wo.diff then
-            return '[c'
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to previous hunk' })
-
-        -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
-          gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'stage git hunk' })
-        map('v', '<leader>hr', function()
-          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'reset git hunk' })
-        -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>hb', function()
-          gs.blame_line { full = false }
-        end, { desc = 'git blame line' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function()
-          gs.diffthis '~'
-        end, { desc = 'git diff against last commit' })
-
-        -- Toggles
-        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
-
-        -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
-      end,
-    },
-  },
-
+  -- {
+  --   -- Adds git related signs to the gutter, as well as utilities for managing changes
+  --   'lewis6991/gitsigns.nvim',
+  --   opts = {
+  --     -- See `:help gitsigns.txt`
+  --     signs = {
+  --       add = { text = '+' },
+  --       change = { text = '~' },
+  --       delete = { text = '_' },
+  --       topdelete = { text = '‾' },
+  --       changedelete = { text = '~' },
+  --     },
+  --     on_attach = function(bufnr)
+  --       local gs = package.loaded.gitsigns
+  --
+  --       local function map(mode, l, r, opts)
+  --         opts = opts or {}
+  --         opts.buffer = bufnr
+  --         vim.keymap.set(mode, l, r, opts)
+  --       end
+  --
+  --       -- Navigation
+  --       map({ 'n', 'v' }, ']c', function()
+  --         if vim.wo.diff then
+  --           return ']c'
+  --         end
+  --         vim.schedule(function()
+  --           gs.next_hunk()
+  --         end)
+  --         return '<Ignore>'
+  --       end, { expr = true, desc = 'Jump to next hunk' })
+  --
+  --       map({ 'n', 'v' }, '[c', function()
+  --         if vim.wo.diff then
+  --           return '[c'
+  --         end
+  --         vim.schedule(function()
+  --           gs.prev_hunk()
+  --         end)
+  --         return '<Ignore>'
+  --       end, { expr = true, desc = 'Jump to previous hunk' })
+  --
+  --       -- Actions
+  --       -- visual mode
+  --       map('v', '<leader>hs', function()
+  --         gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+  --       end, { desc = 'stage git hunk' })
+  --       map('v', '<leader>hr', function()
+  --         gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+  --       end, { desc = 'reset git hunk' })
+  --       -- normal mode
+  --       map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
+  --       map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
+  --       map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
+  --       map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
+  --       map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
+  --       map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
+  --       map('n', '<leader>hb', function()
+  --         gs.blame_line { full = false }
+  --       end, { desc = 'git blame line' })
+  --       map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
+  --       map('n', '<leader>hD', function()
+  --         gs.diffthis '~'
+  --       end, { desc = 'git diff against last commit' })
+  --
+  --       -- Toggles
+  --       map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
+  --       map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+  --
+  --       -- Text object
+  --       map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
+  --     end,
+  --   },
+  -- },
+  --
   -- {
   --   -- Theme inspired by Atom
   --   'navarasu/onedark.nvim',
@@ -291,6 +292,9 @@ require('lazy').setup({
   --     vim.cmd.colorscheme "catppuccin"
   --   end,
   -- },
+  {'dstein64/vim-startuptime'},
+  {'nathom/filetype.nvim'},
+  
 
   {
     -- Set lualine as statusline
@@ -366,6 +370,7 @@ require('lazy').setup({
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
+--
 
 -- Set highlight on search
 vim.o.hlsearch = false
@@ -379,7 +384,7 @@ vim.o.mouse = 'a'
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+-- vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -409,12 +414,15 @@ vim.o.termguicolors = true
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- vim.keymap.set({'n'}, '<Leader>la', '+y', {silent = true })
+vim.keymap.set({'n'}, '<C-c>', '"+y$')
+vim.keymap.set({'v'}, '<C-c>', '"+y')
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+-- vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+-- vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+-- vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
 
 
 -- Diagnostic keymaps
@@ -761,9 +769,4 @@ cmp.setup {
   },
 }
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 etc
---
---
--- -- Uncomment the following lines to use the default colorscheme
 
